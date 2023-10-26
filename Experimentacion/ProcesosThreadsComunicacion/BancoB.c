60a5a5d04121d6a89e3fcd1a98fb24d9ce4b7ae9
Sabrina <sabribarrion@gmail.com>
2023-10-26 11:43:33 -0300

    BancoB completo

--- Experimentacion/ProcesosThreadsComunicacion/BancoB.c
+++ Experimentacion/ProcesosThreadsComunicacion/BancoB.c
@@ -3,14 +3,15 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/ipc.h>
+#include <time.h>
 #include <sys/msg.h>
 
 #define maxCapacidadFilas 15
 #define maxCapacidadMesa 30
 #define cantTipoClientes 3
 const char Clientes[cantTipoClientes] = {'E','U','P'}; //Arreglo con los tipos de clientes.
+int clientesMesa, clientesUsuarios, clientesPoliticos, clientesEmpresas;
 
 void mesa_de_entrada(int msg_idMesa);
 void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idUsu);
@@ -22,40 +23,32 @@ struct message {
 };
 const int size = sizeof(struct message) - sizeof(long);
 
-
 int main(){
-    key_t keyMesa, keyEmp, keyPol, keyUsu;
     int msg_idMesa, msg_idEmp, msg_idPol, msg_idUsu;
 
-    // Generar una clave única
-    keyMesa = ftok("mesa", 'A');
-    keyEmp = ftok("empresas", 'B');
-    keyPol = ftok("politicos", 'C');
-    keyUsu = ftok("usuarios", 'D');
-
     // Creamos las colas de mensajes
-    msg_idMesa = msgget(keyMesa, 0666 | IPC_CREAT);
+    msg_idMesa = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     if (msg_idMesa == -1) {
         printf("Error al crear la cola de mensajes de la mesa de entrada.\n");
         fflush(stdout);
         return 1;
     }
 
-    msg_idEmp = msgget(keyEmp, 0666 | IPC_CREAT);
+    msg_idEmp = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     if (msg_idEmp == -1) {
         printf("Error al crear la cola de mensajes de la fila de empresas.\n");
         fflush(stdout);
         return 1;
     }
 
-    msg_idPol = msgget(keyPol, 0666 | IPC_CREAT);
+    msg_idPol = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     if (msg_idPol == -1) {
         printf("Error al crear la cola de mensajes de la fila de politicos.\n");
         fflush(stdout);
         return 1;
     }
 
-    msg_idUsu = msgget(keyUsu, 0666 | IPC_CREAT);
+    msg_idUsu = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     if (msg_idUsu == -1) {
         printf("Error al crear la cola de mensajes de la fila de usuarios.\n");
         fflush(stdout);
@@ -98,46 +91,56 @@ int main(){
     }
 
     wait(NULL); // Espera a los procesos hijos
+    msgctl(msg_idMesa,IPC_RMID,NULL); //Elimina las colas
+    msgctl(msg_idEmp,IPC_RMID,NULL);
+    msgctl(msg_idPol,IPC_RMID,NULL);
+    msgctl(msg_idUsu,IPC_RMID,NULL);
     return 0;
 }
 
 void mesa_de_entrada(int msg_idMesa) {
+    srand(time(NULL));
     char tipo_cliente = ' ';
     int num_random = 0;
+    clientesMesa = 0;
     while(1) {
         num_random = rand() % 3; // Generamos un numero random entre 0 y 2
         tipo_cliente = Clientes[num_random]; // Usamos el numero random para simular un nuevo cliente
+        printf("Llego un cliente de tipo %c al banco.\n",tipo_cliente);
         struct message m = {1, tipo_cliente}; // Mensaje que vamos a enviar
         struct msqid_ds info; // Variable que usaremos para comprobar la cantidad de mensajes en la cola
         if (msgctl(msg_idMesa, IPC_STAT, &info) == -1) {
             printf("Error al obtener información de la cola de mensajes.\n");
             exit(1);
         }
-        if(info.msg_qnum < maxCapacidadMesa) { // Verificamos que la cantidad de mensajes en la cola sea menor a 30 
-            if(msgsnd(msg_idMesa, &m, size, 0) == -1){
+        clientesMesa = info.msg_qnum; // Numero de clientes en la mesa de entrada
+        if(clientesMesa < maxCapacidadMesa) { // Verificamos que la cantidad de mensajes en la cola sea menor a 30 
+            if(msgsnd(msg_idMesa, &m, size, 0) == -1){ // Ingresa un cliente a la mesa de entrada
                 printf("Error al escribir el mensaje %c\n",tipo_cliente);
                 exit(1);
             }
-            printf("Ingresa un cliente de tipo %c a la mesa de entrada.\n",tipo_cliente);
+            clientesMesa++;
+            printf("Ingresa un cliente de tipo %c a la mesa de entrada.\nClientes en Mesa de Entrada: %d.\n",tipo_cliente,clientesMesa);
             fflush(stdout);
         } else { // Si la mesa de entrada esta llena
             printf("Cliente se retira porque la mesa de entrada está llena.\n");
             fflush(stdout);
         }
-        printf("Hay %s clientes en la mesa de entrada.\n",info.msg_qnum);
-        fflush(stdout);
-        sleep(5);
+        sleep(1);
     }
     exit(0);
 }
 
 void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idUsu) {
-    sleep(1);
+    sleep(5);
+    clientesPoliticos = 0;
+    clientesEmpresas = 0;
+    clientesUsuarios = 0;
     while(1){
         struct message m; // Variable que va a guardar el mensaje recibido
         struct msqid_ds info; // Variable que usaremos para comprobar la cantidad de clientes en las filas
-        if(msgrcv(msg_idMesa, &m, size, 1, 0) == -1){
-			printf("Error al leer el mensaje %s\n",m.cliente);
+        if(msgrcv(msg_idMesa, &m, size, 1, 0) == -1){ // Recibe un cliente de la mesa de entrada
+			printf("Error al leer el mensaje %c\n",m.cliente);
 			fflush(stdout);
             exit(1);
 		}
@@ -147,16 +150,24 @@ void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idU
                     printf("Error al obtener información de la cola de mensajes.\n");
                     exit(1);
                 }
-                if(info.msg_qnum < maxCapacidadFilas) {
-                    if(msgsnd(msg_idEmp, &m, size, 0) == -1){
-                        printf("Error al escribir el mensaje %s\n",m.cliente);
+                clientesEmpresas = info.msg_qnum; // Numero de clientes esperando en la cola de empresas
+                if(clientesEmpresas < maxCapacidadFilas) { // Comprueba se la fila esta llena
+                    if(msgsnd(msg_idEmp, &m, size, 0) == -1){ // Despacha al cliente a la fila de empresas
+                        printf("Error al escribir el mensaje %c\n",m.cliente);
                         exit(1);
+                    } else {
+                        clientesMesa--;
+                        clientesEmpresas++;
+                        printf("Ingresa un cliente a la fila de empresas.\nClientes en la fila de Empresas: %d.\n",clientesEmpresas);
+                        fflush(stdout);
                     }
-                    printf("Ingresa un cliente a la fila de empresas.\n");
-                    fflush(stdout);
                 } else { // Si la fila de empresas esta llena
                     printf("Fila de empresas llena, el cliente espera en la entrada.\n");
                     fflush(stdout);
+                    if(msgsnd(msg_idMesa, &m, size, 0) == -1){ // El cliente vuelve a la mesa de entrada
+                        printf("Error al escribir el mensaje %c\n",m.cliente);
+                        exit(1);
+                    }
                 }
                 break;
             }
@@ -165,16 +176,24 @@ void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idU
                     printf("Error al obtener información de la cola de mensajes.\n");
                     exit(1);
                 }
-                if(info.msg_qnum < maxCapacidadFilas) {
-                    if(msgsnd(msg_idUsu, &m, size, 0) == -1){
-                        printf("Error al escribir el mensaje %s\n",m.cliente);
+                clientesUsuarios = info.msg_qnum; // Numero de clientes esperando en la cola de usuarios
+                if(clientesUsuarios < maxCapacidadFilas) { // Comprueba si la fila esta llena
+                    if(msgsnd(msg_idUsu, &m, size, 0) == -1){ // Despacha al cliente a la fila de Usuarios
+                        printf("Error al escribir el mensaje %c\n",m.cliente);
                         exit(1);
+                    } else {
+                        clientesUsuarios++;
+                        clientesMesa--;
+                        printf("Ingresa un cliente a la fila de usuarios.\nClientes en la fila de Usuarios: %d.\n",clientesUsuarios);
+                        fflush(stdout);
                     }
-                    printf("Ingresa un cliente a la fila de usuarios.\n");
-                    fflush(stdout);
                 } else { // Si la fila de usuarios esta llena
                     printf("Fila de usuarios llena, el cliente espera en la entrada.\n");
                     fflush(stdout);
+                    if(msgsnd(msg_idMesa, &m, size, 0) == -1){ // El cliente vuelve a la mesa de entrada
+                        printf("Error al escribir el mensaje %c\n",m.cliente);
+                        exit(1);
+                    }
                 }
                 break;
             }
@@ -183,21 +202,29 @@ void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idU
                     printf("Error al obtener información de la cola de mensajes.\n");
                     exit(1);
                 }
-                if(info.msg_qnum < maxCapacidadFilas) {
+                clientesPoliticos = info.msg_qnum; // Numero de clientes esperando en la cola de politicos
+                if(clientesPoliticos < maxCapacidadFilas) {
                     if(msgsnd(msg_idPol, &m, size, 0) == -1){
                         printf("Error al escribir el mensaje %s\n",m.cliente);
                         exit(1);
+                    } else {
+                        clientesPoliticos++;
+                        clientesMesa--;
+                        printf("Ingresa un cliente a la fila de politicos.\nClientes en la fila de Politicos: %d.\n",clientesPoliticos);
+                        fflush(stdout);
                     }
-                    printf("Ingresa un cliente a la fila de empresas.\n");
-                    fflush(stdout);
                 } else { // Si la fila de politicos esta llena
                     printf("Fila de politicos llena, el cliente espera en la entrada.\n");
                     fflush(stdout);
+                    if(msgsnd(msg_idMesa, &m, size, 0) == -1){ // El cliente vuelve a la mesa de entrada
+                        printf("Error al escribir el mensaje %c\n",m.cliente);
+                        exit(1);
+                    }
                 }
                 break;
             }
         }
-        sleep(5);
+        sleep(1);
     }
     exit(0);
 }
@@ -206,41 +233,27 @@ void filas_especificas(int msg_idMesa, int msg_idEmp, int msg_idPol, int msg_idU
  * Empleado 2 atiende Usuarios
  * Los 3 empleados atienden Politicos*/
 void empleados(int i, int msg_idEmp, int msg_idPol, int msg_idUsu) {
-    sleep(5);
+    sleep(10); // Delay grande para permitir que las filas se vayan llenando antes de comenzar a atender
     struct message m;
     while (1) {
-        // Como los politicos son prioridad, revisamos siempre primero su fila
-        if(msgrcv(msg_idPol, &m, size, 1, 0) == -1){
-			printf("No hay ningun cliente de prioridad esperando ser atendido.\n");
-			fflush(stdout);
-            continue;
-		} else {
+        if(msgrcv(msg_idPol, &m, size, 1, IPC_NOWAIT) != -1){ // Los tres empleados atienden politicos primero
             printf("Un politico es atendido y se retira.\n");
+            sleep(6);
 			fflush(stdout);
-            // El tiempo que tarda en atenderlo, es un numero random entre 1 y 6
-            sleep(rand() % 6 + 1);
-        }
-
-        if(i == 0 || i == 1) { // Atienden empresas
-            if(msgrcv(msg_idEmp, &m, size, 1, 0) == -1){
-			    printf("Error al leer el mensaje %s\n",m.cliente);
-			    fflush(stdout);
-                exit(1);
-		    } else {
-                printf("Una empresa es atendida y se retira.\n");
-			    fflush(stdout);
-                sleep(rand() % 6 + 1);
+		} else { // No hay politicos para atender. Cuando esto ocurre los empleados atienden las otras filas
+            if(i == 0 || i == 1) { // Empleados 0 y 1: Atienden empresas
+                if(msgrcv(msg_idEmp, &m, size, 1, IPC_NOWAIT) != -1){
+			        printf("Una empresa es atendida y se retira.\n");
+                    fflush(stdout);
+                    sleep(7);
+		        }
+            } else { // Empleado 2: Atiende usuarios
+                if(msgrcv(msg_idUsu, &m, size, 1, IPC_NOWAIT) != -1){
+			        printf("Un usuario es atendido y se retira.\n");
+                    fflush(stdout);
+                    sleep(5);
+                }       
             }
-        } else { // Atiende usuarios
-             if(msgrcv(msg_idUsu, &m, size, 1, 0) == -1){
-			    printf("Error al leer el mensaje %s\n",m.cliente);
-			    fflush(stdout);
-                exit(1);
-		    } else {
-                printf("Un usuario es atendido y se retira.\n");
-			    fflush(stdout);
-                sleep(rand() % 6 + 1);
-            }       
         }
     }
     exit(0);  
